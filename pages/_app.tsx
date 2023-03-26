ee7e2843fd9aa6817894908c06bbbea891ad3d41
James Earthman <james.earthman@gmail.com>
2023-03-26 13:07:11 -0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import "styles/globals.css";
+import "material-icons/iconfont/material-icons.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
