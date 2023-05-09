98115ade63e6c750ea8b0801d594ce511cf31bd5
James Earthman <james.earthman@gmail.com>
2023-05-08 20:05:21 -0500

    worker and chat

---
+++ pages/api/chat.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const message = req.body.message || {};
+
+  if (!message) {
+    return res.status(400).json({
+      error: "Missing chat message",
+    });
+  }
+
+  //get user demographic context
+  if (message === "demo") {
+    return res.status(200).json({
+      message:
+        "The sun slowly sank below the horizon, casting a kaleidoscope of colors across the sky, as a gentle breeze rustled through the verdant leaves of the ancient oak tree. In the distance, the rhythmic sound of waves crashing against the shore filled the air with a sense of serenity. A curious fox cautiously emerged from the underbrush, its amber eyes reflecting the fading light, and began to explore the twilight world. The aroma of a distant campfire mingled with the earthy scent of the forest, creating a symphony of sensations. As the first stars began to twinkle in the ever-darkening sky, the world seemed to hold its breath, waiting for the mysteries of the night to unfold.",
+    });
+  }
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-4",
+    messages: [{ role: "user", content: message }],
+  });
+
+  return res.status(200).json({
+    message: completion.data.choices[0].text,
+  });
+}
--- pages/worker.tsx
+++ pages/worker.tsx
@@ -1,17 +1,72 @@
+import { useState, FormEvent } from "react";
 import { Input } from "@ds/input";
 import { Button } from "@ds/button";
+import { type } from "os";
+import { FadeInText } from "./components/design-system/fade-in-text";
 
-export default function Worker(){
+export default function Worker() {
+  interface ChatMessage {
+    type: "input" | "response";
+    text: string;
+  }
+
+  const [input, setInput] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
+  const [chatLog, setChatLog] = useState<ChatMessage[]>([]);
+
+  async function sendMessage() {
+    setChatLog((prevChatLog) => [
+      ...prevChatLog,
+      { type: "input", text: input },
+    ]);
+
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw (
+        data.error || new Error(`Request failed with status ${response.status}`)
+      );
+    }
+
+    setChatLog((prevChatLog) => [
+      ...prevChatLog,
+      { type: "response", text: data.message },
+    ]);
+  }
+
   return (
     <>
       <div className="flex h-screen bg-slate-500">
         <div className="flex flex-col w-2/5">test1</div>
         <div className="flex flex-col w-3/5">
-        <div className="flex flex-col h-4/5">test2</div>
+          <div className="flex flex-col h-4/5 overflow-y-auto px-5">
+            {chatLog.map((chatMessage, index) => (
+              <>
+                {chatMessage.type === "input" && (
+                  <FadeInText key={index} text="" isReady />
+                )}
+              </>
+            ))}
+          </div>
           <div className="flex flex-col h-1/5 justify-center">
             <div className="flex w-4/5">
-              <Input type="text" className="border-r-0 rounded-r-none w-full"></Input>
-              <Button intent="inner-form" className="border-l-0 rounded-l-none"><span className="material-icons">send</span></Button>
+              <Input
+                type="text"
+                onChange={(event) => setInput(event.currentTarget.value)}
+                className="border-r-0 rounded-r-none w-full"
+              ></Input>
+              <Button
+                intent="inner-form"
+                onClick={sendMessage}
+                className="border-l-0 rounded-l-none"
+              >
+                <span className="material-icons">send</span>
+              </Button>
             </div>
           </div>
         </div>
