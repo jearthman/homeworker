f911263c035e882535854f0a8c126065c55f2a4e
jearthman <james.earthman@gmail.com>
2023-09-29 11:47:38 -0500

    stupid debugLog mistake

--- redis/redis-server-helpers.ts
+++ redis/redis-server-helpers.ts
@@ -1,11 +1,10 @@
 import kvChatClient from "./redis-clients";
 import { Chat } from "@prisma/client";
 import { debugLog } from "../utils/server-helpers";
-import { debuglog } from "util";
 
 export async function setChat(chatId: string, chat: Chat) {
   try {
-    debuglog(`Setting chat ${chatId} in Redis: ${kvChatClient}`);
+    debugLog(`Setting chat ${chatId} in Redis: ${kvChatClient}`);
     return await kvChatClient.set(chatId, chat);
   } catch (error: any) {
     console.error("Error setting chat:", error.message);
@@ -15,7 +14,7 @@ export async function setChat(chatId: string, chat: Chat) {
 
 export async function getChat(chatId: string) {
   try {
-    debuglog(`Getting chat ${chatId} from Redis: ${kvChatClient}`);
+    debugLog(`Getting chat ${chatId} from Redis: ${kvChatClient}`);
     return await kvChatClient.get(chatId);
   } catch (error: any) {
     console.error("Error getting chat:", error.message);
