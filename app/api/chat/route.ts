e46c1d4e96d876dfeedaff849f11e9cf30a4d136
jearthman <james.earthman@gmail.com>
2024-11-07 20:41:44 -0600

    updated model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -146,7 +146,7 @@ export async function POST(req: Request) {
           );
         }
         return openai.chat.completions.create({
-          model: "gpt-3.5-turbo-0613",
+          model: "gpt-3.5-turbo",
           stream: true,
           messages: messages,
           functions,
